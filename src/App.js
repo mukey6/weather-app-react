defa1e0031912717c80ed9f225f599580fb6e8e6
muktar ahmed <muktar.ahmed13@yahoo.com>
2022-05-30 22:51:54 -0500

    added temp and condition

--- src/App.js
+++ src/App.js
@@ -1,59 +1,57 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 const api = {
-  key:'0fb48fb704090281a9b83e51e7d0084b',
-  base:'https://api.openweathermap.org/data/2.5/weather?q='
-
-}
-// {base}{city}&units=imperial&appid={key} {}
+  key: "0fb48fb704090281a9b83e51e7d0084b",
+  base: "https://api.openweathermap.org/data/2.5/weather?q=",
+};
 function App() {
-  // const [date, setDate]=useState()
   const current = new Date();
-  const date = `${current.getMonth()+1}/${current.getDate()}/${current.getFullYear()}`;
+  const date = `${
+    current.getMonth() + 1
+  }/${current.getDate()}/${current.getFullYear()}`;
 
+  const [city, cityQuery] = useState("");
+  const [weather, weatherQuery] = useState();
 
-  const [city, cityQuery]=useState('')
-  const [weather, weatherQuery]=useState()
-
-  // if event (evt), fetch  
-  const search = evt =>{
-    if(evt.key === 'Enter'){
+  // if event (evt) "enter" takes place, fetch the result
+  const search = (evt) => {
+    if (evt.key === "Enter") {
       fetch(`${api.base} ${city}&units=imperial&appid=${api.key} `)
-      .then(res => res.json())
-      .then(result => {
-        cityQuery('')
-        weatherQuery(result)
-      console.log(result)
-      // console.log(weather)
-
-      })
+        .then((res) => res.json())
+        .then((result) => {
+          cityQuery("");
+          weatherQuery(result);
+          console.log(result);
+          // console.log(weather)
+        });
     }
-  }
+  };
   return (
     <div className="App">
       <main>
-      
-        <div className='search-box'>
-          <input type='text' placeholder='search' className='search-bar' 
-          onChange={e=> cityQuery(e.target.value)}
-          value={city}
-          onKeyPress={search}
+        <div className="search-box">
+          <input
+            type="text"
+            placeholder="search"
+            className="search-bar"
+            onChange={(e) => cityQuery(e.target.value)}
+            value={city}
+            onKeyPress={search}
           />
         </div>
-        {(typeof weather != 'undefined') ? (
-<div>
-<div className='location-box'>
-          <div className='city'> {weather.name}</div>
-          <div className='date'>{date}</div>
-        </div>
-        <div className='weather-box'>
-          <div>{Math.round(weather.main.temp)}°F</div> 
-          <div className='weather'>{weather.weather[0].main}</div>
-
-        </div>
-</div>
-          
-) : ('')}
-       
+        {typeof weather != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="city"> {weather.name}</div>
+              <div className="date">{date}</div>
+            </div>
+            <div className="weather-box">
+              <div>{Math.round(weather.main.temp)}°F</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </main>
     </div>
   );
